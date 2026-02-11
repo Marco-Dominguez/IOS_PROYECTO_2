7549961bfa1a6452b15e4aaa2fa2d06f917bd24b
Marco Dominguez <mardom640@gmail.com>
2026-02-11 07:22:18 -0700

    Initial Commit

---
+++ PROYECTO_2/PROYECTO_2App.swift
@@ -0,0 +1,17 @@
+//
+//  PROYECTO_2App.swift
+//  PROYECTO_2
+//
+//  Created by Marco Dominguez on 11/02/26.
+//
+
+import SwiftUI
+
+@main
+struct PROYECTO_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
